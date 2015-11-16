ff4bc6febf9097c74bf4ace2ed0d949791fb2070
jncz <li.j2ee@gmail.com>
2015-11-16 15:36:40 +0800

    add unitest

--- test/test.js
+++ test/test.js
@@ -1,14 +1,6 @@
 var assert = require("assert");
-define = require('node-requirejs-define');
-
-define.config({
-  baseUrl: __dirname,
-  paths: {
-    'cp': '../js'
-  }
-});
-
 var ResLoader = require('../js/ResLoader');
+
 describe('ResLoader', function() {
   describe('load by require', function () {
     it('should not null', function () {
@@ -17,14 +9,37 @@ describe('ResLoader', function() {
   });
 });
 
-describe("ResLoad Instance",function(){
+describe("ResLoader",function(){
   var loader = new ResLoader();
   beforeEach("renew the ResLoader",function(){
     loader = new ResLoader();
   });  
-  describe('loadImg method', function () {
+  describe('#loadImg', function () {
     it('should exist', function () {
         assert.ok(loader.loadImg);
     });
   });
+  
+  describe('#loadImg', function () {
+    it('with null resource path, should return null', function () {
+        var img = loader.loadImg();
+        console.dir(img);
+        img.then(function(d){
+          assert.ok(d.length == 0);
+        });
+    });
+    
+    it('with valid resource path, should return img obj, type should be Promise', function () {
+        var path = "./imgs/img.jpg";
+        loader.init(path);
+        var img = loader.loadImg();
+        assert.ok(img);
+        img.then(function(d){
+          assert.ok(d.src);
+        });
+    });
+  });
+  
 });
+
+
