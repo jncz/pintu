7c37ffdc64509ad7a5fbb8152bd2fbe2fef2f23b
jncz <li.j2ee@gmail.com>
2015-12-11 22:01:47 +0800

    format

--- js/Boot.js
+++ js/Boot.js
@@ -1,9 +1,9 @@
 define(["cp/CanvasManager"],function(CanvasManager){
     var b = function(){
         this.boot = function(){
-        console.log("boot");
-        var can = new CanvasManager();
-	      can.canvas("body","mycanvas2");
+          console.log("boot");
+          var can = new CanvasManager();
+          can.canvas("body","mycanvas");
         };
     };
     
